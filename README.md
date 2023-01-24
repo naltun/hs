460580100bf2063297ca69e5f13f6d21b639a91e
Noah Altunian <noah@altun.cc>
2023-01-24 10:35:46 -0800

    🦕 Init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# hs
+
+Print HTTP status details
+
+<img src="./example.png" alt="Running hs with HTTP status 201" />
+
+## Usage
+
+From within the project root:
+
+```sh
+deno run --allow-read main.ts [-h|--help] <HTTP status: number>
+```
+
+NOTE: deno.land/x/ hosting coming soon...
+
+## License
+
+With the exception of files in the `codes/` directory, files in this codebase
+are licensed under the terms of the Mozilla Public License, v2.0. Please see
+LICENSE for details.
+
+Every file in the `codes/` directory is copied from the
+[httpstatuses](https://github.com/httpstatuses/httpstatuses/tree/main/contents/codes)
+project. Please see the project's repository for LICENSE details.
---
+++ main.ts
@@ -0,0 +1,61 @@
+import { bold, green } from "https://deno.land/std@0.173.0/fmt/colors.ts";
+
+interface Status {
+  code: number;
+  title: string;
+  text: string;
+  links: Array<string>;
+}
+
+const printHelp = () =>
+  console.log("usage: hs [-h|--help] <HTTP status: number>");
+
+function parseUserArg(): number {
+  if (Deno.args[0] === "-h" || Deno.args[0] === "--help") {
+    printHelp();
+    Deno.exit(0);
+  }
+  if (!parseInt(Deno.args[0])) {
+    printHelp();
+    Deno.exit(1);
+  }
+  return Deno.args[0];
+}
+
+function parseStatusText(text: string): string {
+  let count = 0;
+  const statusText = text.split("\n");
+  let parsedText = "";
+
+  for (let i = 0; i < statusText.length; i++) {
+    if (statusText[i].includes("---")) {
+      count += 1;
+    } else if (count === 2) {
+      parsedText += `\n${statusText[i]}`;
+    }
+  }
+
+  return parsedText.trim();
+}
+
+function parseStatfileText(statfileText: string): Status {
+  const status = {};
+  status.code = /code: (\d+)/.exec(statfileText)[1];
+  status.title = /title: (.*)/.exec(statfileText)[1];
+  status.text = parseStatusText(statfileText);
+  return status;
+}
+
+function printStatus(stat: string): void {
+  const statfileText = new TextDecoder("UTF-8").decode(
+    Deno.readFileSync(`./codes/${stat}.txt`),
+  );
+  const parsedStatus = parseStatfileText(statfileText);
+  for (const attr of Object.keys(parsedStatus)) {
+    console.log(bold(attr) + ": " + green(parsedStatus[attr]));
+  }
+}
+
+if (import.meta.main) {
+  printStatus(parseUserArg());
+}
